dd32ca7af9194ebfdbb83a8bde291c32e2ba61aa
Lorena <lorenadomingos@proton.me>
2026-04-03 03:34:01 -0300

    Add simple-crud project and their configuration files

---
+++ simple-crud/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const item = ref('')
+const items = ref([])
+
+const addItem = () => {
+  if (item.value !== '') {
+    items.value.push(item.value)
+    item.value = ''
+  }
+}
+
+const deleteItem = (index) => {
+  items.value.splice(index, 1)
+}
+
+</script>
+
+<template>
+  <input type="text" v-model="item" @keyup.enter="addItem" />
+  <ul>
+    <li v-for="(content, index) in items" :key="index">
+      {{ content }} 
+      <button @click="deleteItem(index)">Delete</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
