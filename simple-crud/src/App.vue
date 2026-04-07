c485063b669d22a15e34b83e9d6e67ba40a454ee
Lorena <lorenadomingos@proton.me>
2026-04-07 20:22:33 -0300

    Add MyCounter component to App.vue and create MyCounter.vue with counter logic

--- simple-crud/src/App.vue
+++ simple-crud/src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+
+import MyCounter from './components/MyCounter.vue'
 
 const item = ref('')
 const items = ref([])
@@ -14,16 +16,19 @@ const addItem = () => {
 const deleteItem = (index) => {
   items.value.splice(index, 1)
 }
-
 </script>
 
 <template>
   <input type="text" v-model="item" @keyup.enter="addItem" />
   <ul>
     <li v-for="(content, index) in items" :key="index">
-      {{ content }} 
+      {{ content }}
       <button @click="deleteItem(index)">Delete</button>
     </li>
+
+    <!-- Making a component -->
+
+    <MyCounter />
   </ul>
 </template>
 
---
+++ simple-crud/src/components/MyCounter.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const counterValue = ref(0)
+
+</script>
+
+<template>
+    <p>{{ counterValue }}</p>
+    <button @click="counterValue++">Increase?</button>
+</template>
