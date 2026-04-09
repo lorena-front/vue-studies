aaf61fee184b7884f93a0d1d8352216346672ffd
Lorena <lorenadomingos@proton.me>
2026-04-09 19:42:40 -0300

    Refactor MyCounter.vue to implement custom reference and increaseCounter function

--- simple-crud/src/components/MyCounter.vue
+++ simple-crud/src/components/MyCounter.vue
@@ -1,45 +1,67 @@
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from 'vue'
+import { ref, computed, customRef } from 'vue'
 
 const counterValue = ref(0)
 
-const increaseCounter = () => {
-  counterValue.value++;
-}
-
 const decreaseCounter = () => {
-  counterValue.value--
+  counterValue.value--;
 }
 
 const doubleValue = computed(function() {
   return counterValue.value * 2;
 })
 
-let timer = 0;
+// let timer = 0;
 
-const start = () => {
-  timer = setInterval(() => {
-    increaseCounter();
-  }, 1000);
-}
+// const start = () => {
+//   timer = setInterval(() => {
+//     increaseCounter();
+//   }, 1000);
+// }
 
-const stop = () => {
-  clearInterval(timer);
-}
+// const stop = () => {
+//   clearInterval(timer);
+// }
 
-// Lifecycle Methods
-onMounted(() => {
-  start();
+// // Lifecycle Methods
+// onMounted(() => {
+//   start();
+// });
+
+// onUnmounted(() => {
+//   stop();
+// })
+
+// Create a custom reference (customRef)
+const count = customRef((track, trigger) => {
+  let value = 0;  // value will be the variable being tracked,initialized here to 0.
+  return {
+    get() {
+      // Track the dependency when the value is read.
+      track();
+      console.log("get value =", value);
+      return value;
+    },
+    set(newValue) {
+      // Update the value and trigger reactivity.
+      value = newValue;
+      trigger();
+      console.log("set value =", value);
+    }
+  };
 });
 
-onUnmounted(() => {
-  stop();
-})
+const increaseCounter = () => {
+  console.log("A")
+  count.value++;
+  console.log("B")
+}
 </script>
 
 <template>
   <p>{{ counterValue }}</p>
-  <button @click="counterValue++">Increase?</button>
+  <p>{{ count }}</p>
+  <button @click="increaseCounter">Increase?</button>
   <button @click="decreaseCounter">Decrease?</button>
   <p>Double Value? => {{ doubleValue }}</p>
 
