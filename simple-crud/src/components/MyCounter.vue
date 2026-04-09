7be0ed24bb09484ff169df5804e718b6a2a701d3
Lorena <lorenadomingos@proton.me>
2026-04-09 00:00:18 -0300

    Modify MyCounter.vue to include start and stop functions for automatic counter updates, utilizing onMounted and onUnmounted lifecycle methods

--- simple-crud/src/components/MyCounter.vue
+++ simple-crud/src/components/MyCounter.vue
@@ -1,11 +1,46 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, onMounted, onUnmounted } from 'vue'
 
 const counterValue = ref(0)
 
+const increaseCounter = () => {
+  counterValue.value++;
+}
+
+const decreaseCounter = () => {
+  counterValue.value--
+}
+
+const doubleValue = computed(function() {
+  return counterValue.value * 2;
+})
+
+let timer = 0;
+
+const start = () => {
+  timer = setInterval(() => {
+    increaseCounter();
+  }, 1000);
+}
+
+const stop = () => {
+  clearInterval(timer);
+}
+
+// Lifecycle Methods
+onMounted(() => {
+  start();
+});
+
+onUnmounted(() => {
+  stop();
+})
 </script>
 
 <template>
-    <p>{{ counterValue }}</p>
-    <button @click="counterValue++">Increase?</button>
+  <p>{{ counterValue }}</p>
+  <button @click="counterValue++">Increase?</button>
+  <button @click="decreaseCounter">Decrease?</button>
+  <p>Double Value? => {{ doubleValue }}</p>
+
 </template>
